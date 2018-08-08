6bf4f8c8e97e895dbfb1a59f9f241e23ab3fae2f
Katie Maxwell <katie@codeinstitute.net>
2018-08-08 12:21:23 +0000

    added to allowed hosts and installed gunicorn

--- quiz_api/settings.py
+++ quiz_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-quiz-rest-api-kgmaxwell1990.c9users.io']
+ALLOWED_HOSTS = ['django-quiz-rest-api-kgmaxwell1990.c9users.io', 'https://djangoquizapi.herokuapp.com/']
 
 CORS_ORIGIN_ALLOW_ALL=True
 
